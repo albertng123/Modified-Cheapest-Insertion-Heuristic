e288b384c26430a370ccb604416c7f40e295b946
albertng123 <albertbobby94@gmail.com>
2020-01-15 15:34:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finding/ArrayMapList.java
@@ -0,0 +1,27 @@
+package com.example.finding;
+
+public class ArrayMapList {
+    private String Destination;
+    private String IdDestination;
+
+
+    public String getIdDestination() {
+        return IdDestination;
+    }
+
+    public void setIdDestination(String idDestination) {
+        IdDestination = idDestination;
+    }
+
+
+    public String getDestination() {
+        return Destination;
+    }
+
+    public void setDestination(String destination) {
+        Destination = destination;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/finding/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.finding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private AppCompatAutoCompleteTextView autoTextView;
+
+    EditText mOrigin;
+    Button tbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayMapList aMap = new ArrayMapList();
+        autoTextView = (AppCompatAutoCompleteTextView)findViewById(R.id.mOrigin);
+        setContentView(R.layout.activity_main);
+        mOrigin = findViewById(R.id.mOrigin);
+        requestQueue = Volley.newRequestQueue(this);
+        getDestinationId();
+        setDestinationList(aMap);
+    }
+
+    private void setDestinationList(ArrayMapList aMap){
+        List<String> mlist = new ArrayList<>();
+        mlist.add(aMap.getDestination());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,mlist);
+        autoTextView.setThreshold(1);
+        autoTextView.setAdapter(adapter);
+    }
+
+    private void getDestinationId(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        System.out.println("GetIDDestinasi");
+        String url ="https://maps.googleapis.com/maps/api/place/autocomplete/json?";
+        System.out.println(url+"&input="+"sunPlaza medan"+"&key="+"AIzaSyBT-M_q9mFlTTCM47VVz1X2mQ9O3PefW4Q");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url+"&input="+mOrigin.getText()+"&key="+R.string.Api, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONArray jsonArray = response.getJSONArray("predictions");
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                ArrayMapList aMap = new ArrayMapList();
+                                System.out.println(" id "+jsonObject.getString("id"));
+                                aMap.setDestination(jsonObject.getString("description"));
+                                aMap.setIdDestination(jsonObject.getString("id"));
+
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(request);
+    }
+}
+
+
